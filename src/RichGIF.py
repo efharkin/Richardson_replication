2cbc52c0d9aa983a14ba03406da78254eaa495ac
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-06-03 12:50:55 -0400

    Add stochastic component to GIF simulations and functionality to perform several repetitions in parallel

--- src/RichGIF.py
+++ src/RichGIF.py
@@ -23,27 +23,34 @@ class model(object):
         self.reset = reset  # Post-spike voltage reset
 
 
-    def simulate(self, I, V0, dt = 0.1):
+    def simulate(self, I, I_N, V0, replicates = 1, dt = 0.1):
 
         """
         Simulate voltage.
 
         Returns:
-            Tuple of I, V, I_g1, and spks in vector format.
+            Tuple of I, V, I_g1, spks, and simulation dt.
+
+            I, V, I_g1, and spks are matrices with dimensionality [replicate, time].
         """
 
-        mat = self._simulate(I, V0, self.C, self.g, self.g1, self.tau1, self.theta, self.reset, dt)
+        I = np.tile(I, (replicates, 1))
 
-        V_vec = mat[0, :]
-        I_g1 = mat[1, :]
-        spks = mat[2, :]
+        I_rand = I_N * np.random.normal(size = I.shape)
 
-        return (I, V_vec, I_g1, spks, dt)
+        sim_tensor = self._simulate(I, I_rand, V0, self.C, self.g, self.g1, self.tau1, self.theta, self.reset, dt)
+
+        V_mat = sim_tensor[0, :, :]
+        I_g1 = sim_tensor[1, :, :]
+        spks = sim_tensor[2, :, :]
+
+        return (I, V_mat, I_g1, spks, dt)
 
 
     @staticmethod
-    @nb.jit(nb.float64[:,:](
-        nb.float64[:],
+    @nb.jit(nb.float64[:,:,:](
+        nb.float64[:,:],
+        nb.float64[:,:],
         nb.float64,
         nb.float64,
         nb.float64,
@@ -53,7 +60,7 @@ class model(object):
         nb.float64,
         nb.float64
         ))
-    def _simulate(I, V0, C, g, g1, tau1, theta, reset, dt):
+    def _simulate(I, I_rand, V0, C, g, g1, tau1, theta, reset, dt):
 
         """
         Private numba-accelerated method for simulation.
@@ -61,67 +68,125 @@ class model(object):
         """
 
         # Create vectors to store output
-        V_vec = np.empty(len(I), dtype = np.float64)
-        w_vec = np.empty(len(I), dtype = np.float64)
-        spks = np.zeros(len(I), dtype = np.bool)
+        V_mat = np.empty(I.shape, dtype = np.float64)
+        w_mat = np.empty(I.shape, dtype = np.float64)
+        spks = np.zeros(I.shape, dtype = np.bool)
 
         # Set initial condition
-        V_vec[0] = V0
-        w_vec[0] = V_vec[0]
+        V_mat[:, 0] = V0
+        w_mat[:, 0] = V_mat[:, 0]
 
         # Integrate over time
         t = 0
-        while t < (len(I) - 1):
+        while t < (I.shape[1] - 1):
 
             # Integrate conductance
-            dw_t = (V_vec[t] - w_vec[t]) / tau1 * dt
-            w_vec[t + 1] = w_vec[t] + dw_t
+            dw_t = (V_mat[:, t] - w_mat[:, t]) / tau1 * dt
+            w_mat[:, t + 1] = w_mat[:, t] + dw_t
 
-            # Spike if above threshold
-            if V_vec[t] >= theta:
-                spks[t] = True
-                V_vec[t + 1] = reset
-            else:
-                # Integrate voltage
-                dV_t = (-g * V_vec[t] - g1 * w_vec[t] + I[t]) / C * dt
-                V_vec[t + 1] = V_vec[t] + dV_t
+            # Detect spiking neurons
+            spiking_neurons_t = V_mat[:, t] >= theta
+
+            # Apply spiking rule to spiking neurons
+            spks[spiking_neurons_t, t]          = True
+            V_mat[spiking_neurons_t, t + 1]     = reset
+
+            # Integrate voltage for all neurons,
+            # but only apply to non-spiking cells.
+            dV_t_deterministic = (-g * V_mat[:, t] - g1 * w_mat[:, t] + I[:, t]) / C * dt
+            dV_t_stochastic = I_rand[:, t] / C * np.sqrt(dt)
+            dV_t_total = dV_t_deterministic + dV_t_stochastic
+
+            V_mat[~spiking_neurons_t, t + 1] = V_mat[~spiking_neurons_t, t] + dV_t_total[~spiking_neurons_t]
 
             # Increment t
             t += 1
 
-        # Return output in a matrix
-        return np.array([V_vec, g1 * w_vec, spks])
+        # Return output in a tensor
+        return np.array([V_mat, g1 * w_mat, spks])
 
 
 class simulation(object):
 
-    def __init__(self, I, mod, V0 = 0, dt = 0.1):
+    def __init__(self, I, I_N, mod, replicates = 1, V0 = 0, dt = 0.1):
 
         self._mod = deepcopy(mod) # Attach a copy of model just in case
 
-        I, V_vec, I_g1, spks, dt = self._mod.simulate(I, V0, dt)
+        I, V_mat, I_g1, spks, dt = self._mod.simulate(I, I_N, V0, replicates, dt)
 
         self.I      = I         # Injected current (nA)
-        self.V      = V_vec     # Somatic voltage (mV)
+        self.V      = V_mat     # Somatic voltage (mV)
         self.I_g1   = I_g1      # Current passed by conductance g1 (nA)
         self.spks   = spks      # Boolean vector of spks
         self.dt     = dt        # Simulation timestep
 
 
     ### Methods to get various transformations of spiketrain
-    def get_spk_vec(self):
+    def get_spk_mat(self):
+
+        """
+        Return a boolean spk matrix ([replicate, time] dimensionality).
+        """
+
         return self.spks
 
     def get_spk_inds(self):
-        return np.where(self.spks)[0]
+
+        """
+        Return a list of arrays with spk inds for each replicate.
+        """
+
+        spk_inds_ls = []
+
+        for i in range(self.replicates):
+            spk_inds_ls.append(np.where(self.spks[i, :])[0])
+
+        return spk_inds_ls
 
     def get_spk_times(self):
-        return self.get_spk_inds() * self.dt
+
+        """
+        Return a list of arrays with spk times in ms.
+        """
+
+        spk_times_ls = self.get_spk_inds()
+
+        for i in range(self.replicates):
+            spk_times_ls[i] = spk_times_ls[i].astype(np.float64) * self.dt
+
+        return spk_times_ls
 
 
     ### Method to get time vector
     def get_t_vec(self):
-        return np.arange(0, int(len(self.I) * self.dt), self.dt)
+
+        """
+        Return a time support vector for any one replicate.
+        """
+
+        return np.arange(0, int(self.I.shape[1] * self.dt), self.dt)
+
+    def get_t_mat(self):
+
+        """
+        Return a time support matrix of the same shape as simulation.I, simulation.V, etc.
+        """
+
+        return np.tile(self.get_t_vec(), (self.I.shape[0], 1))
+
+
+    ### Method to get replicates
+    @property
+    def replicates(self):
+
+        inferred_replicates = [self.I.shape[0],
+                               self.V.shape[0],
+                               self.I_g1.shape[0],
+                               self.spks.shape[0]]
+
+        assert all([inferred_replicates[0] == r for r in inferred_replicates]), 'Not all attrs have same no of replicates'
+
+        return inferred_replicates[0]
 
 
     ### Plotting methods
@@ -131,23 +196,23 @@ class simulation(object):
 
         spec = plt.GridSpec(4, 1, height_ratios = [0.2, 1, 1, 0.5], hspace = 0.4)
 
-        t = self.get_t_vec()
+        t_mat_transpose = self.get_t_mat().T
 
         I_plot = plt.subplot(spec[3, :])
-        plt.plot(t, self.I)
+        plt.plot(t_mat_transpose, self.I.T, 'k-', alpha = 1/self.replicates)
         plt.ylabel('I (nA)')
 
         plt.subplot(spec[0, :], sharex = I_plot)
-        plt.plot(t, self.spks)
+        plt.plot(t_mat_transpose, self.spks.T, 'k-', alpha = 1/self.replicates)
         plt.ylabel('Spks')
         plt.ylim(-0.05, 1.05)
 
         plt.subplot(spec[1, :], sharex = I_plot)
-        plt.plot(t, self.V)
+        plt.plot(t_mat_transpose, self.V.T, 'k-', alpha = 1/self.replicates)
         plt.ylabel('V (mV)')
 
         plt.subplot(spec[2, :], sharex = I_plot)
-        plt.plot(t, self.I_g1)
+        plt.plot(t_mat_transpose, self.I_g1.T, 'k-', alpha = 1/self.replicates)
         plt.ylabel('I_g1 (nA)')
 
         plt.show()
--- tests/RichGIF_test.py
+++ tests/RichGIF_test.py
@@ -20,30 +20,34 @@ dt      = 0.1 #ms
 
 test_mod = rGIF.model(C, g, g1, tau1, theta, reset)
 
+no_neurons = 10
 t = np.arange(0, 1000, dt)
 per = np.linspace(1000/2, 1000/7, len(t))
 test_current = 0.024 * np.sin(t * 2 * np.pi / per) + 0.95
+I_N = 0.11
 
-I, V_vec, I_g1, spks, dt_ = test_mod.simulate(test_current, 0, dt)
+I, V_mat, I_g1, spks, dt_ = test_mod.simulate(test_current, I_N, 0, no_neurons, dt)
 
 # Make figure of simulation output
 plt.figure(figsize = (10, 5))
 
+t_mat = np.tile(t, (no_neurons, 1))
+
 spks_plot = plt.subplot(411)
-plt.plot(t, spks)
+plt.plot(t_mat.T, spks.T, 'k-', alpha = 1/no_neurons)
 plt.ylim(-0.05, 1.05)
 plt.ylabel('Spike status')
 
 plt.subplot(412, sharex = spks_plot)
-plt.plot(t, V_vec)
+plt.plot(t_mat.T, V_mat.T, 'k-', alpha = 1/no_neurons)
 plt.ylabel('V (mV)')
 
 plt.subplot(413, sharex = spks_plot)
-plt.plot(t, I_g1)
+plt.plot(t_mat.T, I_g1.T, 'k-', alpha = 1/no_neurons)
 plt.ylabel('I_g1 (nA)')
 
 plt.subplot(414, sharex = spks_plot)
-plt.plot(t, I)
+plt.plot(t_mat.T, I.T, 'k-', alpha = 1/no_neurons)
 plt.ylabel('I (nA)')
 plt.xlabel('Time (ms)')
 
@@ -52,8 +56,8 @@ plt.show()
 
 #%% TEST SIMULATION CLASS
 
-test_sim = rGIF.simulation(test_current, test_mod)
+test_sim = rGIF.simulation(test_current, I_N, test_mod, no_neurons)
+test_sim.get_spk_mat()
 test_sim.get_spk_inds()
 test_sim.get_spk_times()
-test_sim.get_spk_vec()
 test_sim.basic_plot()

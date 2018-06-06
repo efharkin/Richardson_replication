f4aeb03b218732122d9fc0b07f510473a1dd6c8e
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-06-06 10:44:59 -0400

    Create a new parent class Simulation and make RichGIF.simulation and RichCond.simulation inherit from it
    Consolidate methods that are exactly the same in RichGIF and RichCond implementations of the simulation class into a new Simulation class (in src/SimulationParent.py). Passes all existing tests.

--- src/RichGIF.py
+++ src/RichGIF.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numba as nb
 
+import sys
+sys.path.append('./src')
+
+from SimulationParent import Simulation
+
 
 #%% DEFINE GIF CLASS
 
@@ -107,7 +112,7 @@ class model(object):
         return np.array([V_mat, g1 * w_mat, spks])
 
 
-class simulation(object):
+class simulation(Simulation):
 
     def __init__(self, I, I_N, mod, replicates = 1, V0 = 0, dt = 0.1):
 
@@ -120,98 +125,6 @@ class simulation(object):
         self.I_g1   = I_g1      # Current passed by conductance g1 (nA)
         self.spks   = spks      # Boolean vector of spks
         self.dt     = dt        # Simulation timestep
-
-
-    ### Methods to get various transformations of spiketrain
-    def get_spk_mat(self):
-
-        """
-        Return a boolean spk matrix ([replicate, time] dimensionality).
-        """
-
-        return self.spks
-
-    def get_spk_inds(self):
-
-        """
-        Return a list of arrays with spk inds for each replicate.
-        """
-
-        spk_inds_ls = []
-
-        for i in range(self.replicates):
-            spk_inds_ls.append(np.where(self.spks[i, :])[0])
-
-        return spk_inds_ls
-
-    def get_spk_times(self):
-
-        """
-        Return a list of arrays with spk times in ms.
-        """
-
-        spk_times_ls = self.get_spk_inds()
-
-        for i in range(self.replicates):
-            spk_times_ls[i] = spk_times_ls[i].astype(np.float64) * self.dt
-
-        return spk_times_ls
-
-    def get_firing_rate(self, bin_width = 10, return_I = False):
-
-        """
-        Compute binned firing rate
-
-        Inputs:
-
-            bin_width (float or None)
-            --  Width of bin (ms) to use for firing rate calculation. Binned by timestep if set to None.
-
-        Returns:
-            Tuple of bin centres and binned firing rate.
-        """
-
-        inst_firing_rate = self.spks.sum(axis = 0) / (self.replicates * self.dt) * 1e3
-
-        if bin_width is None:
-            if not return_I:
-                return (self.get_t_vec(), inst_firing_rate)
-            else:
-                return (self.get_t_vec(), inst_firing_rate, self.I.mean(axis = 0))
-        else:
-            firing_rate_bins = np.arange(0, self.get_t_vec()[-1] + bin_width - self.dt, bin_width)
-            binned_firing_rate, bin_edges, _ = stats.binned_statistic(
-                self.get_t_vec(), inst_firing_rate, 'mean', firing_rate_bins
-                )
-
-            fr_bin_centres = (firing_rate_bins[1:] + firing_rate_bins[:-1]) / 2
-
-            if not return_I:
-                return (fr_bin_centres, binned_firing_rate)
-            else:
-                binned_I, _, _ = stats.binned_statistic(
-                self.get_t_vec(), self.I.mean(axis = 0), 'mean', firing_rate_bins
-                )
-
-                return (fr_bin_centres, binned_firing_rate, binned_I)
-
-
-    ### Method to get time vector
-    def get_t_vec(self):
-
-        """
-        Return a time support vector for any one replicate.
-        """
-
-        return np.arange(0, self.I.shape[1] * self.dt, self.dt)
-
-    def get_t_mat(self):
-
-        """
-        Return a time support matrix of the same shape as simulation.I, simulation.V, etc.
-        """
-
-        return np.tile(self.get_t_vec(), (self.I.shape[0], 1))
 
 
     ### Method to get replicates

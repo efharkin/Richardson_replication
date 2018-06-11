f6b882e0cc6be71266e258d2294e3d7beb836082
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-06-10 21:36:41 -0400

    fig 3: Tweaks to improve presentation

--- replicated_figures/fig3.py
+++ replicated_figures/fig3.py
@@ -120,6 +120,13 @@ with open(PICKLE_PATH + 'fig3_mod2analysis.pyc', 'wb') as f:
     pickle.dump(mod2_analysis, f)
 
 
+#%% UNPICKLE ANALYSIS
+
+with open(PICKLE_PATH + 'fig3_mod1analysis.pyc', 'rb') as f:
+    mod1_analysis = pickle.load(f)
+with open(PICKLE_PATH + 'fig3_mod2analysis.pyc', 'rb') as f:
+    mod2_analysis = pickle.load(f)
+
 #%% ASSEMBLE FIGURE
 
 save_path = './doc/img/fig3.png'
@@ -127,9 +134,21 @@ save_path = './doc/img/fig3.png'
 
 plt.rc('text', usetex = True)
 
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
 plt.figure(figsize = (6, 5))
 
-spec = gs.GridSpec(2, 2, hspace = 0.5, wspace = 0.4)
+spec = gs.GridSpec(2, 2, hspace = 0.5, wspace = 0.4, top = 0.95, right = 0.95)
 
 mod1amp_plot = plt.subplot(spec[0, 0])
 mod1amp_plot.set_xscale('log')

cef9a7cfb6f50e60a100f0a19237c8a213c44d83
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-06-11 13:44:02 -0400

    Fixes to fig8.py, pickle fig8 simulations, and initial commit of figure
    Fix stimulus size exponentiation bug in fig8.Analysis(). Fix pickle syntax error in fig8.py. Write code to generate figure 8.

--- replicated_figures/fig8.py
+++ replicated_figures/fig8.py
@@ -5,6 +5,8 @@ import multiprocessing as mp
 import gc
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 
 import sys
 import os
@@ -13,7 +15,6 @@ sys.path.append('./src')
 sys.path.append('./replicated_figures')
 
 import RichCond as Cond
-
 
 
 #%% PERFORM SIMULATIONS
@@ -32,7 +33,7 @@ noise_A = Cond.synaptic_noise(0.01, 0.0005, 3, 0.0085, 0.0005, 10)
 noise_B = Cond.synaptic_noise(0.01, 0.01, 3, 0.015, 0.01, 10)
 
 
-#%%
+#%% DEFINE ANALYSIS CLASS
 
 class Analysis(object):
 
@@ -71,10 +72,6 @@ class Analysis(object):
 
         # Generate enough synaptic noise for the longest simulation.
         # Will be subsetted for shorter simulations to reduce compute time.
-        if verbose:
-            print('Realizing noise.')
-        no_noise_timesteps = 50000#int(no_cycles * 1e3 / (min(freqs) * dt))
-        synaptic_noise.realize((no_neurons, no_noise_timesteps), dt)
 
         # Get gain at each frequency.
         for freq in freqs:
@@ -111,13 +108,12 @@ class Analysis(object):
                 print('Simulating frequency {}Hz'.format(freq))
 
             t = np.arange(0, no_cycles_internal * 1e3 / freq, dt)
-            t = np.tile(t, (no_neurons, 1))
-
             oscillating_current = I1 * np.sin(t * 2 * np.pi * freq * 1e-3) + I0
+            synaptic_noise.realize((no_neurons, len(oscillating_current)), dt)
 
             mod_sim = Cond.simulation(oscillating_current, V0, mod, no_neurons,
-                ge = synaptic_noise.ge[:, :oscillating_current.shape[1]], Ee = 0,
-                gi = synaptic_noise.gi[:, :oscillating_current.shape[1]], Ei = -75,
+                ge = synaptic_noise.ge[:, :oscillating_current.shape[0]], Ee = 0,
+                gi = synaptic_noise.gi[:, :oscillating_current.shape[0]], Ei = -75,
                 dt = dt)
 
             if verbose:
@@ -237,9 +233,113 @@ inputs = [
 ]
 
 if __name__ == '__main__':
-    p = mp.Pool(2)
+    p = mp.Pool(4)
     results = p.map(worker, inputs)
 
-    with open('results_dump.pyc', 'wb') as f:
-        pickle.dump(f)
+    with open('./replicated_figures/fig8_results_dump.pyc', 'wb') as f:
+        pickle.dump(results, f)
         f.close()
+
+#%% UNPICKLE SIMULATIONS
+
+with open('./replicated_figures/fig8_results_dump.pyc', 'rb') as f:
+    results = pickle.load(f)
+
+mod1_noiseA = results[0]
+mod1_noiseB = results[1]
+mod2_noiseA = results[2]
+mod2_noiseB = results[3]
+
+del results
+
+#%% COMPENSATE FOR >2PI PHASE SHIFTS
+
+for mod in [mod1_noiseA, mod1_noiseB, mod2_noiseA, mod2_noiseB]:
+    for i in range(len(mod.phases)):
+        if mod.phases[i] > np.pi:
+            mod.phases[i] -= 2 * np.pi
+
+
+#%% MAKE FIGURE
+
+save_path = './doc/img/fig8.png'
+
+markersize = 1.2
+
+plt.rc('text', usetex = True)
+
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
+plt.figure(figsize = (6, 4))
+
+spec = gs.GridSpec(2, 2, right = 0.95, top = 0.95, wspace = 0.35, hspace = 0.6)
+
+# Mod 1
+plt.subplot(spec[0, 0])
+plt.title('\\textbf{{A1}} Model 1 gain', loc = 'left')
+plt.axvline(6.5, linestyle = 'dotted', linewidth = 0.7, color = 'k')
+plt.axvline(50, linestyle = '--', dashes = (10, 10), linewidth = 0.7, color = 'k')
+plt.plot(mod1_noiseA.freqs, mod1_noiseA.gains / mod1_noiseA.gains.max(),
+    'o', color = (0.8, 0.2, 0.2), markersize = markersize)
+plt.plot(mod1_noiseB.freqs, mod1_noiseB.gains / mod1_noiseB.gains.max(),
+    'o', color = (0.2, 0.2, 0.8), markersize = markersize)
+plt.annotate(
+    'Resonance', xy = (6.5, 0.35), xytext = (15, -10), ha = 'left', va = 'center',
+    textcoords = 'offset points', arrowprops = {'arrowstyle': '->'}
+)
+plt.annotate(
+    '$r_0$', xy = (50, 0.35), xytext = (-20, -5), ha = 'right', va = 'center',
+    textcoords = 'offset points', arrowprops = {'arrowstyle': '->'}
+)
+plt.ylabel('Gain')
+plt.xlabel('Frequency (Hz)')
+
+plt.subplot(spec[0, 1])
+plt.title('\\textbf{{A2}} Model 1 phase shift', loc = 'left')
+plt.axvline(6.5, linestyle = 'dotted', linewidth = 0.7, color = 'k')
+plt.axvline(50, linestyle = '--', dashes = (10, 10), linewidth = 0.7, color = 'k')
+plt.plot(mod1_noiseA.freqs, 360 * mod1_noiseA.phases / (2 * np.pi),
+    'o', color = (0.8, 0.2, 0.2), markersize = markersize, label = 'Low noise')
+plt.plot(mod1_noiseB.freqs, 360 * mod1_noiseB.phases / (2 * np.pi),
+    'o', color = (0.2, 0.2, 0.8), markersize = markersize, label = 'High noise')
+plt.ylabel('Phase shift (radians)')
+plt.xlabel('Frequency (Hz)')
+plt.legend()
+
+# Mod 2
+plt.subplot(spec[1, 0])
+plt.title('\\textbf{{B1}} Model 2 gain', loc = 'left')
+plt.axvline(30, linestyle = 'dotted', linewidth = 0.7, color = 'k')
+plt.axvline(50, linestyle = '--', dashes = (10, 10), linewidth = 0.7, color = 'k')
+plt.plot(mod2_noiseA.freqs, mod2_noiseA.gains / mod2_noiseA.gains.max(),
+    'o', color = (0.8, 0.2, 0.2), markersize = markersize)
+plt.plot(mod2_noiseB.freqs, mod2_noiseB.gains / mod2_noiseB.gains.max(),
+    'o', color = (0.2, 0.2, 0.8), markersize = markersize)
+plt.ylabel('Gain')
+plt.xlabel('Frequency (Hz)')
+
+plt.subplot(spec[1, 1])
+plt.title('\\textbf{{B2}} Model 2 phase shift', loc = 'left')
+plt.axvline(30, linestyle = 'dotted', linewidth = 0.7, color = 'k')
+plt.axvline(50, linestyle = '--', dashes = (10, 10), linewidth = 0.7, color = 'k')
+plt.plot(mod2_noiseA.freqs, 360 * mod2_noiseA.phases / (2 * np.pi),
+    'o', color = (0.8, 0.2, 0.2), markersize = markersize)
+plt.plot(mod2_noiseB.freqs, 360 * mod2_noiseB.phases / (2 * np.pi),
+    'o', color = (0.2, 0.2, 0.8), markersize = markersize)
+plt.ylabel('Phase shift (radians)')
+plt.xlabel('Frequency (Hz)')
+
+if save_path is not None:
+    plt.savefig(save_path, dpi = 300)
+
+plt.show()

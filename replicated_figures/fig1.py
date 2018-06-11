b52c26a7abb0ce132abd8a6bc7bf38bf5a29f366
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-06-10 21:36:22 -0400

    fig 1: Tweaks to improve presentation

--- replicated_figures/fig1.py
+++ replicated_figures/fig1.py
@@ -40,16 +40,28 @@ save_path = './doc/img/fig1.png'
 
 plt.rc('text', usetex = True)
 
-plt.figure(figsize = (6, 6))
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
 
-spec = gs.GridSpec(3, 1, height_ratios = [1.3, 1, 1], hspace = 0.4)
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
+
+plt.figure(figsize = (6, 5))
+
+spec = gs.GridSpec(3, 1, height_ratios = [1.3, 1, 1], hspace = 0.4, top = 0.95, right = 0.95)
 spec_inner = gs.GridSpecFromSubplotSpec(2, 1, spec[0, :], hspace = 0, height_ratios = [4, 1])
 
 
 t_mat_transpose = fig1_GIFsim.get_t_mat().T
 
 sample_neuron_plot = plt.subplot(spec_inner[0, :])
-plt.title('\\textbf{{A.}} Sample trace', loc = 'left')
+plt.title('\\textbf{{A}} Sample trace', loc = 'left')
 V_trace = fig1_GIFsim.V.T[:, 0] - 70
 V_trace[fig1_GIFsim.spks[0, :]] = 0
 plt.plot(t_mat_transpose[:, 0], V_trace, 'k-', linewidth = 0.5)
@@ -57,12 +69,19 @@ pltools.add_scalebar(anchor = (0, 0.1), omit_x = True, y_units = 'mV')
 
 sample_current_plot = plt.subplot(spec_inner[1, :])
 plt.plot(t_mat_transpose[:, 0], 1e3 * fig1_GIFsim.I.T[:, 0], 'k-', linewidth = 0.5)
+plt.annotate(
+'$I_1$',
+xy = (210, 1e3 * 0.955),
+xytext = (20, -5), textcoords = 'offset points',
+ha = 'center', va = 'top',
+arrowprops = {'arrowstyle': '->'}
+)
 pltools.add_scalebar(anchor = (0, 0.02), x_label_space = 0.1, bar_space = 0.04,
 x_units = 'ms', y_units = 'pA', x_on_left = False)
 
 
 raster_plot = plt.subplot(spec[1, :])
-plt.title('\\textbf{{B.}} Raster plot', loc = 'left')
+plt.title('\\textbf{{B}} Raster plot', loc = 'left')
 spk_times = fig1_GIFsim.get_spk_times()
 for rep in range(fig1_GIFsim.replicates):
     plt.plot(spk_times[rep], [rep] * len(spk_times[rep]), '|', color = 'k', markersize = 1)
@@ -70,7 +89,7 @@ raster_plot.set_xticklabels([])
 plt.ylabel('Neuron')
 
 firing_rate_plot = plt.subplot(spec[2, :])
-plt.title('\\textbf{{C.}} Mean firing rate', loc = 'left')
+plt.title('\\textbf{{C}} Mean firing rate', loc = 'left')
 t_binned, binned_firing_rate = fig1_GIFsim.get_firing_rate(bin_width = bin_width)
 plt.bar(t_binned, binned_firing_rate, width = bin_width, facecolor = 'none', edgecolor = 'k')
 plt.axhline(binned_firing_rate.mean(),
